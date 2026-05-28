8ef5ab021ce36757e399c8f68256e9d20d975d87
Dhruv Soni <dhruvsoni15176@gmail.com>
2026-05-28 16:53:11 +0530

    img to Image/next

--- components/Client.tsx
+++ components/Client.tsx
@@ -3,7 +3,7 @@
 import { companies, testimonials } from "@/data";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 import React from "react";
-
+import Image from "next/image";
 
 const Client = () => {
 	return (
@@ -35,12 +35,12 @@ const Client = () => {
           {companies.map((company) => (
             <React.Fragment key={company.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
-                <img
+                <Image
                   src={company.img}
                   alt={company.name}
                   className="md:w-10 w-5"
                 />
-                <img
+                <Image
                   src={company.nameImg}
                   alt={company.name}
                   width={company.id === 4 || company.id === 5 ? 100 : 150}
